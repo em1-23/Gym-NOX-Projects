a2b15cadebb18897b0a25360e109ef43d201c735
m_ahmoud <melngar650@gmail.com>
2026-06-09 15:11:51 +0300

    Adding main Style

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,21 @@ import React from "react";
 function App(){
 	return(
 		<div className="Gym-Pages">
-			
+			<button className="Button">Button Test</button>
+			<button className="Button Spectial">Button Test</button>
+			<ul className="ul-List">
+				<li>NOX</li>
+				<li>NOX</li>
+				<li>NOX</li>
+				<li>NOX</li>
+				<li>NOX</li>
+			</ul>
+			<div className="Rotate-Shape">
+				<span>18.2K</span><br />
+				<span className="Description">
+					Follower 
+				</span>
+			</div>
 		</div>
 	);
 }
